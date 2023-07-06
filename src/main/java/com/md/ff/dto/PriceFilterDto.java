9b1f544de2af1b08ad9b2c65723692cc38bdb8a7
elmekia <mati.dela@gmail.com>
2023-07-06 19:55:45 -0300

    ft: @NotNull added to filters and mockMvc config

--- src/main/java/com/md/ff/dto/PriceFilterDto.java
+++ src/main/java/com/md/ff/dto/PriceFilterDto.java
@@ -2,12 +2,15 @@ package com.md.ff.dto;
 
 import static com.md.ff.utils.ValidationPatternConstant.LOCAL_DATE_TIME_REGEX;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 public record PriceFilterDto(
-    @Pattern(regexp = LOCAL_DATE_TIME_REGEX, message = "Invalid date format.") String date,
+    @Pattern(regexp = LOCAL_DATE_TIME_REGEX, message = "Invalid date format.") @NotNull String date,
+    @NotNull
     @Positive Integer productId,
+    @NotNull
     @Positive Integer brandId) {
 
 }
