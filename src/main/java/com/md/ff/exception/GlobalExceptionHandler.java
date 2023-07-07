bb2d67e1c60647fa22aab5bff9d575b5e610d51b
elmekia <mati.dela@gmail.com>
2023-07-06 21:37:11 -0300

    ft: remove unused import

--- src/main/java/com/md/ff/exception/GlobalExceptionHandler.java
+++ src/main/java/com/md/ff/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,18 +16,6 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<Map<String, String>> handleValidationExceptions(
       MethodArgumentNotValidException ex) {
-    BindingResult bindingResult = ex.getBindingResult();
-    Map<String, String> errors = new HashMap<>();
-
-    for (FieldError fieldError : bindingResult.getFieldErrors()) {
-      errors.put(fieldError.getField(), fieldError.getDefaultMessage());
-    }
-
-    return ResponseEntity.badRequest().body(errors);
-  }
-
-  @ExceptionHandler(BindException.class)
-  public ResponseEntity<Map<String, String>> handleBindExceptions(BindException ex) {
     BindingResult bindingResult = ex.getBindingResult();
     Map<String, String> errors = new HashMap<>();
 
